3ca95c099f43394400ccf81a0f9a3f903988a47c
svg83 <svgeneraloff@gmail.com>
2023-03-23 07:01:58 +0400

    добавил список собственников на страницу с квартирой

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Сomplaint, Owner
 
 
+class OwnersInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ['owner']
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
@@ -12,6 +17,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+    inlines = [OwnersInline]
 
 
 @admin.register(Сomplaint)
