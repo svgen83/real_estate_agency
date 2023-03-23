f578ca52cc445f4a9950da913c36440ffcbb5127
svg83 <svgeneraloff@gmail.com>
2023-03-23 06:49:12 +0400

    сделал связь квартиры-собственники

--- property/admin.py
+++ property/admin.py
@@ -8,7 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
     list_display = ['address', 'price', 'new_building', 
-                    'construction_year', 'town', 'owner_pure_phone']
+                    'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
