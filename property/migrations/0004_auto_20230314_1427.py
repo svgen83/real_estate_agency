35fd4789ea2c0b247b628149adacd253b2cbb3ac
svg83 <svgeneraloff@gmail.com>
2023-03-28 16:28:03 +0400

    сократил количество запросов к бд

--- property/migrations/0004_auto_20230314_1427.py
+++ property/migrations/0004_auto_20230314_1427.py
@@ -5,10 +5,10 @@ from django.db import migrations
 
 def choose_new_buildings(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        flat.save()
+    Flat.objects.filter(construction_year__gte=2015
+                        ).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015
+                        ).update(new_building=False)
 
 
 class Migration(migrations.Migration):
